9e9af8316ddfd2b3ecfc6d75f32326770ce19117
AnthonyLuo1998 <jum1274001055@gmail.com>
2024-05-14 20:15:58 +0800

    feat:重复代码封装

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import a_farg from "./shader/index.frag";
 import a_vert from "./shader/index.vert";
+import { createProgram, createShader } from "./utils";
 
 const canvas = document.getElementById("cvs");
 
 const gl = canvas.getContext("webgl");
 
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, a_vert);
-gl.compileShader(vertexShader);
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, a_vert);
 
-const fragementShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragementShader, a_farg);
-gl.compileShader(fragementShader);
+const fragementShader = createShader(gl, gl.FRAGMENT_SHADER, a_farg);
 
-const program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragementShader);
-
-gl.linkProgram(program);
-gl.useProgram(program);
+const program = createProgram(gl, vertexShader, fragementShader);
 
 gl.clearColor(0.0, 0.0, 0.0, 1);
 gl.clear(gl.COLOR_BUFFER_BIT);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,31 @@
-function createShader(gl, type) {}
+/**
+ * 创建着色器
+ *
+ * @param gl WebGL渲染上下文
+ * @param type 着色器类型（gl.VERTEX_SHADER 或 gl.FRAGMENT_SHADER）
+ * @param source 着色器源代码
+ * @returns 返回创建的着色器对象
+ */
+export function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  return shader;
+}
 
-function createProgram() {}
+/**
+ * 创建WebGL程序对象
+ *
+ * @param gl WebGL上下文对象
+ * @param vertexShader 顶点着色器对象
+ * @param fragementShader 片段着色器对象
+ * @returns 创建的WebGL程序对象
+ */
+export function createProgram(gl, vertexShader, fragementShader) {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragementShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+  return program;
+}
