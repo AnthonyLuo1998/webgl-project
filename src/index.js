29b2ff03ec6f04bc1fe0cb143365908df5426b35
AnthonyLuo1998 <jum1274001055@gmail.com>
2024-06-03 17:17:07 +0800

    feat:画布点击三次成三角

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, a_farg);
 
 const program = createProgram(gl, vertexShader, fragmentShader);
 
-const points = [1.0, 0.2, 0.3, 0.4, 0.5, 0.6];
+const points = [];
 
 // 获取a_position变量的地址
 const a_position = gl.getAttribLocation(program, "a_position");
+// 获取screen变量的职位
+const a_screen_size = gl.getAttribLocation(program, "a_screen_size");
+// 赋值
+gl.vertexAttrib2f(a_screen_size, canvas.width, canvas.height);
 
 // 创建缓冲区对象
 const buffer = gl.createBuffer();
@@ -23,16 +27,27 @@ const buffer = gl.createBuffer();
 // 绑定缓冲区对象
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
-// 缓冲区数据
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
-
 // 缓冲区数据与a_position绑定
 gl.enableVertexAttribArray(a_position);
 
 // 指定a_position变量的数据格式
 gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
 
+canvas.addEventListener("click", (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  points.push(x, y);
+
+  // 一个点包含x,y两个坐标，所以三个点成一个三角形，所以需要提供6个坐标
+  if (points.length % 6 === 0) {
+    console.log("draw");
+    console.log(points);
+    // 缓冲区数据
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, points.length / 2);
+  }
+});
+
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
-
-gl.drawArrays(gl.TRIANGLES, 0, 3);
--- src/shader/triangle/triangle.vert
+++ src/shader/triangle/triangle.vert
@@ -2,7 +2,11 @@ precision mediump float;
 
 attribute vec2 a_position;
 
+attribute vec2 a_screen_size;
+
 void main(){
-    gl_Position = vec4(a_position, 0.0, 1.0);
+    vec2 position = a_position / a_screen_size * 2.0 - 1.0;
+    position = position * vec2(1.0, -1.0);
+    gl_Position = vec4(position, 0.0, 1.0);
     gl_PointSize = 10.0;
 }
